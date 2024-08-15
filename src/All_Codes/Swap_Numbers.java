7c43b01c31f5b9006efe3d34de665727576fdb8b
Megha <meghasha1607@gmail.com>
2024-08-15 14:58:59 +0530

    Swap two numbers

---
+++ src/All_Codes/Swap_Numbers.java
@@ -0,0 +1,27 @@
+package All_Codes;
+
+public class Swap_Numbers {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 30;
+        System.out.println("a = " + a + ", b = " + b);
+
+//        Using 3rd variable
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println("a = " + a + ", b = " + b);
+
+//        Without using 3rd variable
+//        a = a+b;
+//        b = a-b;
+//        a = a-b;
+//        System.out.println("a = " + a + ", b = " +
+
+//        NOTE - Comment the above method to see the results of 2nd method
+
+    }
+
+}
