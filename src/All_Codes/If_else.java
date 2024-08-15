04f15a110509c725e431caeaea1c10e260cc44a7
Megha <meghasha1607@gmail.com>
2024-08-15 14:49:59 +0530

    Eligibility for vote

---
+++ src/All_Codes/If_else.java
@@ -0,0 +1,17 @@
+package All_Codes;
+
+public class If_else {
+
+    public static void main(String[] args) {
+
+        int age = 25;
+
+        if (age >= 18){
+            System.out.println("You are eligible to give vote");
+        }else{
+            System.out.println("You are not eligible to give vote");
+        }
+
+    }
+
+}
